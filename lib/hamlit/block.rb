fe26a60d0aa60a7f5a045431224375b317c6a926
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-01-12 21:41:13 +0900

    bundle gem hamlit-block

---
+++ lib/hamlit/block.rb
@@ -0,0 +1,7 @@
+require "hamlit/block/version"
+
+module Hamlit
+  module Block
+    # Your code goes here...
+  end
+end
---
+++ spec/hamlit/block_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Hamlit::Block do
+  it 'has a version number' do
+    expect(Hamlit::Block::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
