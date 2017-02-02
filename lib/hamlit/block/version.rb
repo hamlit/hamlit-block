92b15fbf7cc65e34b617a4f6d2584123300b8ad2
Takashi Kokubun <takashi-kokubun@cookpad.com>
2017-02-03 08:20:41 +0900

    Version 0.4.0 for the future

--- lib/hamlit/block/version.rb
+++ lib/hamlit/block/version.rb
@@ -1,5 +1,14 @@
 module Hamlit
   module Block
-    VERSION = "0.3.1"
+    VERSION = "0.4.0"
+
+    begin
+      require 'cell/hamlit/version'
+    rescue LoadError
+    else
+      if Gem::Version.new(Cell::Hamlit::VERSION) <= Gem::Version.new('0.2')
+        raise NotImplementedError.new('hamlit-block does not support cells-hamlit <= 0.2. See https://github.com/trailblazer/cells-hamlit/issues/6')
+      end
+    end
   end
 end
