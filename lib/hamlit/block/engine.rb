815b0f68725ae8d9133da74fc035ec2bae6621da
Takashi Kokubun <takashi-kokubun@cookpad.com>
2017-02-03 07:50:35 +0900

    Duplicate Hamlit::Engine with hamlit-block's default

--- lib/hamlit/block/engine.rb
+++ lib/hamlit/block/engine.rb
@@ -1,7 +1,32 @@
+require 'hamlit/engine'
+
 module Hamlit
   module Block
-    class Engine < ::Hamlit::Engine
-      options[:escape_html] = false
+    class Engine < Temple::Engine
+      define_options(
+        :buffer_class,
+        generator:    Temple::Generators::ArrayBuffer,
+        format:       :html,
+        attr_quote:   "'",
+        escape_html:  false,
+        escape_attrs: true,
+        autoclose:    %w(area base basefont br col command embed frame
+                         hr img input isindex keygen link menuitem meta
+                         param source track wbr),
+        filename:     "",
+      )
+
+      use Hamlit::Parser
+      use Hamlit::Compiler
+      use Hamlit::HTML
+      use Hamlit::StringSplitter
+      use Hamlit::StaticAnalyzer
+      use Hamlit::Escapable
+      use Hamlit::ForceEscapable
+      filter :ControlFlow
+      filter :MultiFlattener
+      filter :StaticMerger
+      use :Generator, -> { options[:generator] }
     end
   end
 end
